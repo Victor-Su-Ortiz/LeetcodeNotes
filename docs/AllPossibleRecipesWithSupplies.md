8fc2457d9be6a4b38dacdf1b6262d1ac1a35232b
vsuortiz-cmu-S25 <vas64@cornell.edu>
2025-03-21 12:14:38 -0700

    add daily for all recipes that can be made with supplies

--- docs/AllPossibleRecipesWithSupplies.md
+++ docs/AllPossibleRecipesWithSupplies.md
@@ -1,14 +1,24 @@
 # AllPossibleRecipesWithSupplies
 
 ## Problem Description
-[PROBLEM_DESCRIPTION]
+You have information about n different recipes. You are given a string array recipes and a 2D string array ingredients. The ith recipe has the name recipes[i], and you can create it if you have all the needed ingredients from ingredients[i]. A recipe can also be an ingredient for other recipes, i.e., ingredients[i] may contain a string that is in recipes.
+
+You are also given a string array supplies containing all the ingredients that you initially have, and you have an infinite supply of all of them.
+
+Return a list of all the recipes that you can create. You may return the answer in any order.
+
+Note that two recipes may contain each other in their ingredients.
 
 ## Approach
-[DESCRIBE_YOUR_APPROACH]
+There is a depedency relationship between the ingredients and the recipes. Some ingredients may be recipes so their ingredients must be available. Since supplies are infinite we can begin by checking what they can make.
+
+This can be reduced to kahns algorithm. Initially the supplies have no dependency so they can be explored first. Whatever recipe is completed by them can be made. We then check what the next layer of recipes we can create with our current available supplies and recipes at this layer.
 
 ## Time Complexity
-- Time Complexity: [TIME_COMPLEXITY]
-- Space Complexity: [SPACE_COMPLEXITY]
+- Time Complexity: O(n + E)
+- Space Complexity: O(n)
+
+We visit every node once. and we visit every edge once so O(N + E) where N is the number of ingredients which is recipes + supplies
 
 ## Notes
 [ANY_ADDITIONAL_NOTES]
--- src/AllPossibleRecipesWithSupplies.cpp
+++ src/AllPossibleRecipesWithSupplies.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <string>
 #include <unordered_map>
+#include <unordered_set>
 #include <algorithm>
 #include "solution.h"
 
@@ -11,14 +12,59 @@
 class AllPossibleRecipesWithSuppliesSolution : public Solution {
 public:
     // Your solution implementation here
+    std::vector<std::string> findAllRecipes(std::vector<std::string>& recipes, std::vector<std::vector<std::string>>& ingredients, std::vector<std::string>& supplies) {
+        std::unordered_map<std::string, std::vector<std::string>> graph;
+        std::unordered_set<std::string> isRecipe;
+        std::unordered_map<std::string, int> indegree;
+
+        for (const auto& recipe: recipes) {
+            isRecipe.insert(recipe);
+        }
+        for (int i = 0; i < (int) ingredients.size(); i++) {
+            for (const auto& food: ingredients[i]) {
+                graph[food].push_back(recipes[i]);
+                indegree[recipes[i]]++;
+            }
+        }
+        std::queue<std::string> q;
+        for (const auto& supply: supplies) {
+            q.push(supply);
+        }
+        std::vector<std::string> ans;
+
+        while(!q.empty()) {
+            auto cur = q.front();
+            q.pop();
+            if (isRecipe.find(cur) != isRecipe.end()) {
+                ans.push_back(cur);
+            }
+            for (const auto& neighbor: graph[cur]) {
+                if (indegree.find(neighbor) != indegree.end() && --indegree[neighbor] == 0) {
+                    indegree.erase(neighbor);
+                    q.push(neighbor);
+                }
+            }
+        }
+        return ans;
+    }
     
     void run() override {
         // Test cases
         std::cout << "Running test cases for AllPossibleRecipesWithSupplies..." << std::endl;
+        std::vector<std::string> recipes = {"bread","sandwich","burger"};
+        std::vector<std::vector<std::string>> ingredients = {{"yeast","flour"}, {"bread","meat"}, {"sandwich","meat","bread"}};
+        std::vector<std::string> supplies = {"yeast","flour","meat"};
         
         // Example test case
         // auto result = yourSolutionMethod(params);
         // std::cout << "Result: " << result << std::endl;
+        auto result = findAllRecipes(recipes, ingredients, supplies);
+        std::cout << "Output: ";
+        for (const auto& recipe: result) {
+            std::cout << recipe << " ";
+        }
+        std::cout << std::endl;
+
     }
 };
 
