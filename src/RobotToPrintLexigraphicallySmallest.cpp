8e4780747886ddbddf5cc3da07ff68117fdc1068
vsuortiz-cmu-S25 <vsuortiz1@gmail.com>
2025-06-06 13:22:22 -0700

    add problem for robot

--- src/RobotToPrintLexigraphicallySmallest.cpp
+++ src/RobotToPrintLexigraphicallySmallest.cpp
@@ -12,14 +12,36 @@ using namespace std;
 class RobotToPrintLexigraphicallySmallestSolution : public Solution {
 public:
     // Your solution implementation here
+    string robotWithString(string s) {
+        vector<int> counts(26);
+        for (const auto& c: s) {
+            counts[c - 'a']++;
+        }
+        
+        stack<char> st;
+        int smallestIdx = 0;
+        string ans = "";
+        for (const auto& c: s) {
+            st.push(c);
+            counts[c - 'a']--;
+            while (smallestIdx < 26 && counts[smallestIdx] == 0) {
+                smallestIdx++;
+            }
+            while (!st.empty() && (st.top() - 'a') <= smallestIdx) {
+                ans += st.top();
+                st.pop();
+            }
+        }
+        return ans;
+    }
     
     void run() override {
         // Test cases
         std::cout << "Running test cases for RobotToPrintLexigraphicallySmallest..." << std::endl;
-        
+        auto result = robotWithString("bdda");
         // Example test case
         // auto result = yourSolutionMethod(params);
-        // std::cout << "Result: " << result << std::endl;
+        std::cout << "Result: " << result << std::endl;
     }
 };
 
